ac0aaafadb74633bff655fe60456ab796c4982de
Ropex037 <86832967+Ropex037@users.noreply.github.com>
2021-07-07 14:40:30 +0200

    Add files via upload

---
+++ html&css/bizz-mizz-fast-site/navbar.js
@@ -0,0 +1,8 @@
+function dropdownMenu() {
+    var x = document.getElementById('dropdownClick');
+    if (x.className === "topnav") {
+        x.className += " responsive";
+    } else {
+        x.className = "topnav";
+    }
+};
